f56373fddeb278419c8bee0717b92ce9a4caff15
Mizanur <jameslev2001@gmail.com>
2022-09-27 16:47:53 +0530

    find middle node & reverse of a linkList(unsolved)

---
+++ middleNode.java
@@ -0,0 +1,53 @@
+public class middleNode {
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+
+        private ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public ListNode head;
+
+    public void dispaly() {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public ListNode middleNode() {
+        if (head == null) {
+            return null;
+        }
+        ListNode slowPtr = head;
+        ListNode firstPtr = head;
+
+        while (firstPtr != null && firstPtr.next != null) {
+            slowPtr = slowPtr.next;
+            firstPtr = firstPtr.next.next;
+        }
+        return slowPtr;
+    }
+
+    public static void main(String[] args) {
+        middleNode mn = new middleNode();
+        mn.head = new ListNode(10);
+        ListNode firstNode = new ListNode(15);
+        ListNode secondNode = new ListNode(2);
+        ListNode thirdNode = new ListNode(12);
+        ListNode forthNode = new ListNode(10);
+
+        mn.head.next = firstNode;
+        firstNode.next = secondNode;
+        secondNode.next = thirdNode;
+        thirdNode.next = forthNode;
+
+        mn.dispaly();
+        System.out.println("middle node is -> " + mn.middleNode().data);
+    }
+}
---
+++ revLinkList.java
@@ -0,0 +1,57 @@
+//not solved yeat
+public class revLinkList {
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+
+        private ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public ListNode head;
+
+    public void display(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public ListNode revarceNode() {
+        if (head == null) {
+            return head;
+        }
+        ListNode current = head;
+        ListNode Next = null;
+        ListNode previous = null;
+        while (current != null) {
+            Next = current.next;
+            current.next = previous;
+            previous = current;
+            current = Next;
+        }
+        return previous;
+    }
+
+    public static void main(String[] args) {
+        revLinkList rll = new revLinkList();
+        rll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode forth = new ListNode(11);
+
+        rll.head.next = second;
+        second.next = third;
+        third.next = forth;
+
+        rll.display(rll.head);
+        rll.revarceNode();
+        ListNode revListHead = rll.revarceNode();
+        rll.head = revListHead;
+        rll.display(revListHead);
+    }
+}
