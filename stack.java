8f1bd581fe22f10d9bf3a3acc1b5e9b7a20360e7
Mizanur <jameslev2001@gmail.com>
2022-10-23 08:16:20 +0530

    implement a Stack

---
+++ stack.java
@@ -0,0 +1,55 @@
+import java.util.EmptyStackException;
+
+public class stack {
+    private class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public ListNode top;
+
+    public void push(int value) {
+        ListNode newNode = new ListNode(value);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public int pop() {
+        int result;
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+
+        top = top.next;
+        result = top.data;
+        return result;
+    }
+
+    public int keep() {
+        if (top == null) {
+            throw new EmptyStackException();
+        }
+        return top.data;
+    }
+
+    public static void main(String[] args) {
+        stack stack = new stack();
+        stack.push(5);
+        stack.push(10);
+        stack.push(115);
+        stack.push(11);
+
+        System.out.println(stack.keep());
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        // stack.pop();
+        System.out.println(stack.keep());
+    }
+
+}
